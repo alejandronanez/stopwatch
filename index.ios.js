51081f4811ad1ca9e75923765948137aa3301361
Alejandro Nanez <alejonanez@gmail.com>
2016-02-24 16:35:36 -0500

    Basic styling + border() to identify each component easy

--- index.ios.js
+++ index.ios.js
@@ -1,10 +1,20 @@
 import React, {
   AppRegistry,
+  StyleSheet,
   Text,
   View
 } from 'react-native';
 
 const StopWatch = React.createClass({
+    /**
+     * Easy to identify components - DEV ONLY
+     */
+    border(color) {
+        return {
+            borderColor: color,
+            borderWidth: 4
+        }
+    },
     lapButton() {
         return (
             <View>
@@ -21,12 +31,39 @@ const StopWatch = React.createClass({
     },
     render() {
         return (
-            <View>
-                <Text>00:00:00</Text>
-                {this.startStopButton()}
-                {this.lapButton()}
+            <View style={styles.container}>{/* container*/}
+                {/* header */}
+                <View style={[styles.header, this.border('yellow')]}>
+                    <View style={this.border('red')}>
+                        <Text>00:00:00</Text>
+                    </View>
+                    <View style={this.border('green')}>
+                        {this.startStopButton()}
+                        {this.lapButton()}
+                    </View>
+                </View>
+                {/* /header */}
+
+                {/* footer */}
+                <View style={[styles.footer, this.border('blue')]}>
+                    <Text>I am a list of laps</Text>
+                </View>
+                {/* footer */}
             </View>
         );
+    }
+});
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, // Fill the entire screen
+        alignItems: 'stretch' // Align items in react goes form <--->
+    },
+    header: {
+        flex: 1
+    },
+    footer: {
+        flex: 1
     }
 });
 
